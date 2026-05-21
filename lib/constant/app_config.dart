f7d2567a6d38c6cf0dc237608ce0a93a70a3de2a
ekiq11 <ahmad.subki1992@gmail.com>
2026-05-21 20:18:36 +0800

    Bump version to 3.0.0

--- lib/constant/app_config.dart
+++ lib/constant/app_config.dart
@@ -5,8 +5,8 @@ class AppConfig {
 
   static const String appName = 'ExamGO';
 
-  // Sync dengan pubspec.yaml: version: 2.1.4+28
-  static const String appVersion = '2.1.4';
+  // Sync dengan pubspec.yaml: version: 3.0.0+31
+  static const String appVersion = '3.0.0';
   static const int qrFormatVersion = 1;
 
   /// Secret key for HMAC-SHA256 QR signing.
